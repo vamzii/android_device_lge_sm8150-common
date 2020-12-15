86655a9d78b1637c549cc63590995de6080d4dd2
Alessandro Astone <ales.astone@gmail.com>
2020-12-15 01:20:29 +0100

    sm8150-common: Add libcomparetf2 shim
    Provide __lttf2 to various blobs
    
    Change-Id: Idb8309fcdc969e9954f0b8b4ca7a39ab261e64de

--- sm8150.mk
+++ sm8150.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
-    Snap \
+    libcomparetf2 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # CNE

72bdba8b6a0bf3edd15bd6b9d9f5b789608cfd87
vamsi <vamsi@tech4tress.xyz>
2021-09-18 04:42:29 +0800

    * fixes logspam: 11-08 09:05:18.838   691   691 E WifiHAL : error_handler_nan: Error code:-95 (Operation not supported on transport endpoint) 11-08 09:05:18.838   691   691 E WifiHAL : nan_disable_request: requestEvent Error:-3 11-08 09:05:18.838   691   691 E WifiHAL : error_handler_nan: Error code:-22 (Invalid argument) 11-08 09:05:18.838   691   691 E WifiHAL : nan_data_interface_delete: requestEvent Error:-5 11-08 09:05:18.838   691   691 E WifiHAL : error_handler_nan: Error code:-22 (Invalid argument) 11-08 09:05:18.838   691   691 E WifiHAL : nan_data_interface_delete: requestEvent Error:-5 11-08 09:05:18.839  1236  3352 E WifiAwareNativeApi: disable: null interface
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- sm8150.mk
+++ sm8150.mk
@@ -325,9 +325,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-#    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
-#    frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
